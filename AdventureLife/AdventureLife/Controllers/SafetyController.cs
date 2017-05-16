90f5fe98dcc1c9f488dd5155fc1621383b6e39cf
Sam Mullen <our.denouement@gmail.com>
2017-05-16 10:40:40 +0200

    base controllers and views added

---
+++ AdventureLife/AdventureLife/Controllers/SafetyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdventureLife.Controllers
+{
+    public class SafetyController : Controller
+    {
+        // GET: Safety
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
