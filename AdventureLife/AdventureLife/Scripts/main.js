c44648cb2b2f4e2b541ec5f540bd02a6a21d7c94
Sam Mullen <our.denouement@gmail.com>
2017-05-17 16:17:09 +0200

    update: html + css applied to public views

---
+++ AdventureLife/AdventureLife/Scripts/main.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#nav-icon").click(function(){$(this).toggleClass("open"),$("#main-nav").toggleClass("open")}),$(".activity-banner i, .activity-banner-2 i, .activity-banner-3 i").click(function(){$("html, body").animate({scrollTop:$(".breadcrumb").offset().top-$(".header").outerHeight()},1500)})});
